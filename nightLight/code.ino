3f5e2e3da10369163c8238ebf13e297a1ea24585
CIRQTRONIX <circuitronics24@gmail.com>
2026-06-10 20:04:38 +0530

    Update code.ino

--- nightLight/code.ino
+++ nightLight/code.ino
@@ -1,1 +1,25 @@
+//.................Automatic Night Light by CIRQTRONIX.................
 
+#define led 9
+#define ldr A0
+
+int threshold = 600; //keep this between 400 and 1000
+
+void setup()
+{
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  int val = analogRead(ldr);
+  if(val > threshold)
+  {
+    digitalWrite(led,HIGH);
+  }
+  else
+  {
+    digitalWrite(led,LOW);
+  }
+  delay(200);
+}
